b408d6c66e49879ac820d9b09f9f51cb8530e2a2
Donald Liu <donaliu@microsoft.com>
2019-08-02 13:54:04 -0700

    Delete SAP Monitor will also unassign and delete CSI if necessary Create SAP Monitor will pass in KeyVault ID as well

--- azext_hanaonazure/_client_factory.py
+++ azext_hanaonazure/_client_factory.py
@@ -11,10 +11,10 @@ def _hana_instance_client_factory(cli_ctx, *_):
 def cf_hanainstance_groups(cli_ctx, *_):
     return _hana_instance_client_factory(cli_ctx).hana_instances
 
-def _msi_client_factory(cli_ctx, **_):
+def _msi_client_factory(cli_ctx, subscription_id=None):
     from azure.mgmt.msi import ManagedServiceIdentityClient
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    return get_mgmt_service_client(cli_ctx, ManagedServiceIdentityClient)
+    return get_mgmt_service_client(cli_ctx, ManagedServiceIdentityClient, subscription_id=subscription_id)
 
 def _keyvault_client_factory(cli_ctx, subscription_id=None):
     from azure.mgmt.keyvault import KeyVaultManagementClient

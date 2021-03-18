92085be50f56114efab4fdc1b60355af7c2faf45
Donald <donaliu@microsoft.com>
2021-03-18 09:44:28 -0700

    Use custom_show_command instead as per guidelines

--- azext_hanaonazure/commands.py
+++ azext_hanaonazure/commands.py
@@ -16,8 +16,7 @@ def load_command_table(self, _):
     with self.command_group('hanainstance', client_factory=cf_hanainstance_groups) as g:
         g.custom_command('create', 'create_hanainstance')
         g.custom_command('list', 'list_hanainstance')
-        g.custom_command('show', 'show_hanainstance',
-                         exception_handler=empty_on_404)
+        g.custom_show_command('show', 'show_hanainstance')
         g.custom_command('restart', 'restart_hanainstance')
         g.custom_command('start', 'start_hanainstance')
         g.custom_command('shutdown', 'shutdown_hanainstance')
@@ -27,7 +26,7 @@ def load_command_table(self, _):
 
     with self.command_group('sapmonitor', client_factory=cf_sapmonitor_groups) as g:
         g.custom_command('list', 'list_sapmonitor')
-        g.custom_command('show', 'show_sapmonitor', exception_handler=empty_on_404)
+        g.custom_show_command('show', 'show_sapmonitor')
         g.custom_command('create', 'create_sapmonitor')
         g.generic_update_command('update', getter_name='show_sapmonitor', setter_name='update_sapmonitor',
                                  command_type=custom_type, supports_no_wait=True)
